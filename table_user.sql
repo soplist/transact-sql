f4526b7309cf374ca2f498d9f17b926737555912
soplist <sopcase@yahoo.com>
2016-09-24 09:18:15 +0800

    20160924

---
+++ table_user.sql
@@ -0,0 +1,21 @@
+use accumulate_points_DB
+
+
+if exists(select * from sysobjects where name='tb_user')
+   drop table tb_user
+go
+
+create table tb_user
+(
+   UserID int identity(1,1) primary key,
+   UserName nvarchar(30),
+   PassWord nvarchar(30),
+   Email nvarchar(50),
+   Role bit
+)
+go
+
+insert into tb_user(UserName,PassWord,Email,Role) values('wangkang1','123456','soplist@hotmail.com',1);
+insert into tb_user(UserName,PassWord,Email,Role) values('wangkang2','123','sopcase@yahoo.com',0);
+
+select * from tb_user;
