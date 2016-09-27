115570274d54f11f4e7f4c155c931c10e8bfe600
soplist <sopcase@yahoo.com>
2016-09-27 10:33:56 +0800

    20160927

--- table_user.sql
+++ table_user.sql
@@ -19,3 +19,6 @@ insert into tb_user(UserName,PassWord,Email,Role) values('wangkang1','123456','s
 insert into tb_user(UserName,PassWord,Email,Role) values('wangkang2','123','sopcase@yahoo.com',0);
 
 select * from tb_user;
+
+update tb_user set Role=1 where UserName='wangkang3';
+update tb_user set Role=0 where UserName='wangkang4';
