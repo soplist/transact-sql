c5c748516fdbac306d9c796bf335087cb96e4fab
soplist <sopcase@yahoo.com>
2016-09-30 08:58:50 +0800

    20160930

--- table_counter_info.sql
+++ table_counter_info.sql
@@ -8,10 +8,12 @@ go
 create table tb_CounterInfo
 (
    ID int identity(1,1) primary key,
-   IP varchar,
+   IP varchar(50),
    LoginTime datetime,
    LeaveTime datetime,
-   Browser varchar,
-   OS varchar
+   Browser varchar(20),
+   OS varchar(20)
 )
 go
+
+insert into tb_CounterInfo(IP,LoginTime,LeaveTime,Browser,OS) values('127.0.0.1','2016/9/30 8:43:27','2016/9/30 8:44:56','Firefox','WinNT');
