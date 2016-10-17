73469e645725d4c82f78a0bf090000a4e1ac8c55
soplist <sopcase@yahoo.com>
2016-10-17 12:34:40 +0800

    20161017

---
+++ points_management_db_4.sql
@@ -0,0 +1,18 @@
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('xieandi','123456',2,0,'Ğ»°²µÏ');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('wangbeijun','123456',2,0,'Íô±¸¾ü');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('guoguang','123456',2,0,'¹ù¹â');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('liufanfan','123456',2,0,'Áõ·¬·­');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('wangdelin','123456',2,0,'ÍôµÂÁÖ');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('wangfei','123456',2,0,'Íô·É');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('wangbenjun','123456',2,0,'Íõ±¾¾ü');
+
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('quxiaoxia','123456',1,0,'ÇüĞ¢Ï¼');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('chenxiaofang','123456',1,0,'³ÂÏş·¼');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('wangli','123456',1,0,'ÍõÀö');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('fanzhengwei','123456',1,0,'·¶ÕıÎ°');
+
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('luona','123456',3,0,'ÂŞÄÈ');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('licaihong','123456',3,0,'Àî²Êºç');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('dengyuying','123456',3,0,'µËÓñÓ¢');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('weimingyue','123456',3,0,'Î¤Ã÷ÔÂ');
+insert into points_management_DB_user(username,passowrd,department_id,role,real_name) values('liuxueyong','123456',3,0,'ÁõÑ§ÓÂ');
